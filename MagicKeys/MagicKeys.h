791f1595cf795ba25825cf0e4961e447926d3a20
Zsolt Szatmari <zsolt.szatmari@me.com>
2012-07-26 08:35:54 +0200

    There are problems with xib vs. prefpane with new Xcode, going step-by-step…

---
+++ MagicKeys/MagicKeys.h
@@ -0,0 +1,15 @@
+//
+//  MagicKeys.h
+//  MagicKeys
+//
+//  Created by Zsolt Szatmari on 7/26/12.
+//  Copyright (c) 2012 Treasure Box. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface MagicKeys : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ MagicKeys/MagicKeys.m
@@ -0,0 +1,17 @@
+//
+//  MagicKeys.m
+//  MagicKeys
+//
+//  Created by Zsolt Szatmari on 7/26/12.
+//  Copyright (c) 2012 Treasure Box. All rights reserved.
+//
+
+#import "MagicKeys.h"
+
+@implementation MagicKeys
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
